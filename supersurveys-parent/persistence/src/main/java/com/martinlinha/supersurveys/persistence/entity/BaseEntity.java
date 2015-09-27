d02c53b6af7f9a73284eff9207b4ae32c0cb5109
martin.linha <martin.linha6@gmail.com>
2015-09-27 16:39:53 +0200

    Init config

---
+++ supersurveys-parent/persistence/src/main/java/com/martinlinha/supersurveys/persistence/entity/BaseEntity.java
@@ -0,0 +1,52 @@
+package com.martinlinha.supersurveys.persistence.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	@Column(updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdOn;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModified;
+
+	@PrePersist
+	public void prePersist() {
+		createdOn = new Date();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		lastModified = new Date();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+}
